dd033475ef337d9c122c8c9e3ba18b822cd27540
KGayatri15 <gayatriinshunya@gmail.com>
2021-01-16 12:06:32 +0530

    Change in function Name

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ function oAuth(){
 }
 //client-id - 1053381465878-vb5nntqvopdnbag9f060pon9d7qh81j4.apps.googleusercontent.com
 //client-secret - Wtewu9zg0q8TlcPC1Des1Na1
+var params = {
+    'client_id': '1053381465878-vb5nntqvopdnbag9f060pon9d7qh81j4.apps.googleusercontent.com',
+    'redirect_uri': 'https://kgayatri15.github.io/GithubIntegration/upload.html',
+    'scope': 'https://www.googleapis.com/auth/drive',
+    'state': 'ActionSpaceEditor',
+    'include_granted_scopes': 'true',
+    'response_type': 'token'
+};
 function GoogleSignIn(){
-    var params = {
-        'client_id': '1053381465878-vb5nntqvopdnbag9f060pon9d7qh81j4.apps.googleusercontent.com',
-        'redirect_uri': 'https://kgayatri15.github.io/GithubIntegration/upload.html',
-        'scope': 'https://www.googleapis.com/auth/drive',
-        'state': 'ActionSpaceEditor',
-        'include_granted_scopes': 'true',
-        'response_type': 'token'
-    };
     var uri = buildEncodedUri(params);
     var url = 'https://accounts.google.com/o/oauth2/v2/auth?';  
     var service = url + uri;
