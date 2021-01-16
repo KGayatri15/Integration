03b96e9082036dc46ce7d6ea8f8a8b5ac9203c97
KGayatri15 <gayatriinshunya@gmail.com>
2021-01-16 18:16:26 +0530

    Params of fileUpload

--- upload.js
+++ upload.js
@@ -6,18 +6,18 @@ window.addEventListener('load',()=>{
 function upload(event){
     event.preventDefault();
     console.log("File Upload API in progress");
-    var url = 'https://www.googleapis.com/upload/drive/v2/files?' ;
+    var url = 'https://www.googleapis.com/upload/drive/v3/files?' ;
     var urlParams = new URLSearchParams(service);
     console.log(urlParams.get('token_type') +" "+ urlParams.get('access_token'));
+    var file = document.getElementById('file').files[0];
     var params = {
         "Authorization":urlParams.get('token_type') +" "+ urlParams.get('access_token'),
-        'uploadType':'media'
+        'uploadType':'media',
+        'Content-Type':file.type,
+        'Content-Length':file.size
     }
-    var file = document.getElementById('file').files[0];
     var formData = new FormData();
-    formData.append("file", file,file.name);
-    formData.append("upload_file", true);
-    console.log("formData.file:- " + formData.file);
+    formData.append("file", file.data,file.name);
     console.log("filename:- " + file.name);
     var service_url = url + buildEncodedUri(params);
     fetch(service_url,{
