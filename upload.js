7c481e2544ff3ac4a779206edbf045ec85b58580
KGayatri15 <gayatriinshunya@gmail.com>
2021-01-18 11:32:01 +0530

    removed onLoad()

--- upload.js
+++ upload.js
@@ -1,25 +1,18 @@
-var service;
-window.addEventListener('load',()=>{
-    const url = new URL(window.location.href);
-    service = unbuildEndodedUri(url);
-    console.log(service);
-})
 function upload(event){
     event.preventDefault();
     console.log("File Upload API in progress");
+    var service = unbuildEndodedUri(window.location.href);
     var urlParams = new URLSearchParams(service.search);
     console.log(urlParams.get('token_type') +" "+ urlParams.get('access_token'));
     var file = document.getElementById('file').files[0];
     var r = new FileReader();
     r.onload = function(){ alert(r.result); };
-    var url = 'https://www.googleapis.com/upload/drive/v2/files?' ;
+    var url = 'https://www.googleapis.com/upload/drive/v3/files?' ;
     var params = {
         "Authorization": urlParams.get('token_type') +" "+ urlParams.get('access_token'),
         'uploadType':'media',
         'Content-Type':file.type,
     }
-    // var formData = new FormData();
-    // formData.append("file", file);
     console.log("filename:- " + file.name);
     var service_url = url + buildEncodedUri(params);
     fetch(service_url,{
